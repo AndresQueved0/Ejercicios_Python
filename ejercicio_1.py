e622ff8a98b2d4fadf41617f44fc643f8cea83e4
AndresQueved0 <quevedo0504@gmail.com>
2024-07-17 22:04:19 -0500

    Ejercicio No.1

---
+++ ejercicio_1.py
@@ -0,0 +1,17 @@
+space_odyssey = "2001"
+ 
+#Ejercicio 1
+#print ("La felicidad se puede encontrar hasta en los mas oscuros")
+
+##print (" momentos, si somos capaces de usar bien la luz.")
+
+#print ("- Albus Dumbledore")
+
+#Forma corta
+print (''' La felicidad se puede encontrar hasta en los mas oscuros
+       momentos, si somos capaces de usar bien la luz.
+       - Albus Dumbledore''')
+
+#Ejercicio 2
+nombre = "Andres Quevedo"
+print("Hola " + nombre + ", un gusto en conocerte!" )
