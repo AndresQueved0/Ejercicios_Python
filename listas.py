c5935e37562efe1fb5628a2053be54e85daa5b49
AndresQueved0 <quevedo0504@gmail.com>
2024-07-31 22:08:10 -0500

    Diccionario en python

---
+++ listas.py
@@ -0,0 +1,77 @@
+#Crear listas
+'''empty_list = []
+
+languages = ['Python', 'Ruby', 'Javascript']
+
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13]
+
+data = ['Tenerife', {'cielo': 'limpio', 'temp': 24}, 3718, (28.2933947, -16.5226597)]'''
+
+#Operaciones con listas
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+print(shopping[0])
+
+shopping[1]
+
+shopping[2]
+
+shopping[-1]  # acceso con índice negativo'''
+
+#Trocear una lista
+'''shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+
+shopping[0:3]
+
+shopping[:3]
+
+shopping[2:4]
+
+shopping[-1:-4:-1]
+
+# Equivale a invertir la lista
+shopping[::-1]
+
+
+
+shopping
+
+shopping[10:]
+
+shopping[-100:2]
+
+shopping[2:100]'''
+
+#invertir una lista
+
+'''shopping
+
+shopping[::-1]
+
+shopping
+
+list(reversed(shopping))'''
+
+
+#Creando desde vacío
+
+'''even_numbers = []
+
+for i in range(20):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+print(even_numbers)'''
+
+#Añadir en cualquier posición de una lista
+
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.insert(1, 'Jamón')
+
+shopping
+
+shopping.insert(3, 'Queso')
+
+shopping'''
+
---
+++ tuplas.py
@@ -0,0 +1,6 @@
+#Creando tuplas
+'''empty_tuple = ()
+
+tenerife_geoloc = (28.46824, -16.25462)
+
+three_wise_men = ('Melchor', 'Gaspar', 'Baltasar')'''
